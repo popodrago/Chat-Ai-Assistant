19330878fb6902a35c137d21b0608421daaead9b
mugisha pacifique <165270993+popodrago@users.noreply.github.com>
2026-04-08 09:11:59 +0200

    Set basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export', // Required for GitHub Pages
   // If you are deploying to https://<username>.github.io/<repository-name>/
   // you MUST set basePath to '/<repository-name>'
-  // basePath: '/your-repo-name', 
+  basePath: '/Chat-Ai-Assistant', 
   trailingSlash: true, // Recommended for GitHub Pages to handle routes correctly
   typescript: {
     ignoreBuildErrors: true,
